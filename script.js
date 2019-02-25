ae01c5956e3fd2b8249601a4ca2c1e64559391e6
Paul <bory12@gmail.com>
2019-02-25 13:48:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
 // ==UserScript==
 // @name         Redmine Closed Removed From Subtasks
 // @namespace    https://github.com/extstopcodepls/redmine_closed_substasks_remover
-// @version      0.2
+// @version      0.4
 // @description  Usuwa linki z podzagadnień, które są zamknięte
 // @author       Paweł Borawski
 // @match        https://redmine.x-code.pl/issues/*
 // @grant        none
 // ==/UserScript==
 
-function getAllRows() {
+function getAllSubtasksRows() {
      return document
             .querySelectorAll('#issue_tree table.list.issues tr.closed');
 }
 
-function show(event) {
-    var toUnblockElements = getAllRows();
+function showSubtasks(event) {
+    var toUnblockElements = getAllSubtasksRows();
     toUnblockElements.forEach(function (toUnblockElement) {
         toUnblockElement.style.display = 'table-row';
     });
     event.target.innerHTML = "Schowaj";
-    event.target.onclick = hide;
+    event.target.onclick = hideSubtasks;
 }
 
-function hide(event) {
-    var toRemoveElements = getAllRows();
+function hideSubtasks(event) {
+    var toRemoveElements = getAllSubtasksRows();
     toRemoveElements
         .forEach(function (closedElement) {
             closedElement.style.display = 'none';
         });
     event.target.innerHTML = "Pokaż";
-    event.target.onclick = show;
+    event.target.onclick = showSubtasks;
 }
 
-(function() {
-    'use strict';
+function setupSubtasks() {
 
-    var toRemoveElements = getAllRows();
+    var toRemoveElements = getAllSubtasksRows();
 
     var subTaskTitleElement =
         document.querySelectorAll('#issue_tree > p > strong')[0];
@@ -44,8 +43,8 @@ function hide(event) {
 
     var showHideElement = document.createElement("a");
     showHideElement.innerHTML = "Pokaż";
-    showHideElement.href = "#";
-    showHideElement.onclick = show;
+    showHideElement.style.cursor = "pointer";
+    showHideElement.onclick = showSubtasks;
 
     subTaskTitleElement.parentNode.insertBefore(showHideElement, subTaskTitleElement.nextSibling);
 
@@ -53,4 +52,58 @@ function hide(event) {
         .forEach(function (closedElement) {
             closedElement.style.display = 'none';
         });
+}
+
+function getAllCorrelatedTasksRows() {
+     return document
+            .querySelectorAll('#relations table.list.issues tr.closed');
+}
+
+function showCorrelatedTasks(event) {
+    var toUnblockElements = getAllCorrelatedTasksRows();
+    toUnblockElements.forEach(function (toUnblockElement) {
+        toUnblockElement.style.display = 'table-row';
+    });
+    event.target.innerHTML = "Schowaj";
+    event.target.onclick = hideCorrelatedTasks;
+}
+
+function hideCorrelatedTasks(event) {
+    var toRemoveElements = getAllCorrelatedTasksRows();
+    toRemoveElements
+        .forEach(function (closedElement) {
+            closedElement.style.display = 'none';
+        });
+    event.target.innerHTML = "Pokaż";
+    event.target.onclick = showCorrelatedTasks;
+}
+
+function setupCorrelatedTasks() {
+
+    var toRemoveElements = getAllCorrelatedTasksRows();
+
+    var subTaskTitleElement =
+        document.querySelectorAll('#relations > p > strong')[0];
+
+    subTaskTitleElement.innerHTML = subTaskTitleElement.innerHTML + " - " + toRemoveElements.length + " zamkniętych. ";
+
+    var showHideElement = document.createElement("a");
+    showHideElement.innerHTML = "Pokaż";
+    showHideElement.style.cursor = "pointer";
+    showHideElement.onclick = showCorrelatedTasks;
+
+    subTaskTitleElement.parentNode.insertBefore(showHideElement, subTaskTitleElement.nextSibling);
+
+    toRemoveElements
+        .forEach(function (closedElement) {
+            closedElement.style.display = 'none';
+        });
+}
+
+(function() {
+    'use strict';
+
+    setupSubtasks();
+
+    setupCorrelatedTasks();
 })();
